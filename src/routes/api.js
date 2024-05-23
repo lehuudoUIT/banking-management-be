a47be12f42843c2174bee938cc8e2c732c40a2f6
lehuudoUIT <22520254@gm.uit.edu.vn>
2024-05-23 12:58:15 +0700

    create getStatistic api

--- src/routes/api.js
+++ src/routes/api.js
@@ -68,6 +68,11 @@ import {
   postUpdateRule,
 } from "../controllers/ruleController";
 
+import {
+  getAllStatistic,
+  getAllSavingRevenueByYear,
+} from "../controllers/dashboardController";
+
 let router = Router();
 
 let initAPIRoutes = (app) => {
@@ -167,6 +172,9 @@ let initAPIRoutes = (app) => {
   router.post("/rule/create", postCreateRule);
   router.post("/rule/delete", postDeleteRule);
   router.post("/rule/update", postUpdateRule);
+  //! Dashboard
+  router.get("/dashboard/statistic", getAllStatistic);
+  router.get("/dashboard/saving-revenue/:year", getAllSavingRevenueByYear);
 
   return app.use("/api/v1/", router);
 };
